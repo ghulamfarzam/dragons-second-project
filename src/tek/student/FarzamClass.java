20f0e6a3fb76349f27ad753bb79cedaba48ea0ed
farzamali <gh.alifarzam@gmail.com>
2023-06-06 16:09:40 -0400

    new

--- src/tek/student/FarzamClass.java
+++ src/tek/student/FarzamClass.java
@@ -4,6 +4,8 @@ public class FarzamClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is Farzam, TekSchool student");
+		System.out.println("this is the second line");
+		System.out.println("this is the third line");
 
 	}
 
